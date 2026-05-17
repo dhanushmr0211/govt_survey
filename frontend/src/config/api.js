8a15410b1d956692d3fecf2ce399811768600793
dhanushmr0211 <1si24cs051>
2026-05-17 09:47:28 +0530

    version 8.4

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,3 +1,3 @@
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://10.73.182.200:3000/api/v1';
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://govt-survey-backend-19218031051.asia-south1.run.app/api/v1';
 
 export default API_BASE_URL;
