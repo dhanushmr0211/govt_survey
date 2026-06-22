3aa693080bc180f4293ef61e42f0287608033f49
dhanushmr0211 <1si24cs051>
2026-06-23 01:49:13 +0530

    fix: allow mobile users to access getProjectStructure backend endpoint

--- backend/src/routes/project.routes.js
+++ backend/src/routes/project.routes.js
@@ -10,6 +10,6 @@ const projectRouter = express.Router();
 projectRouter.use(authenticate);
 
 projectRouter.get('/', listProjects);
-projectRouter.get('/:projectId/structure', requireRole(ROLES.MASTER_ADMIN, ROLES.ADMIN, ROLES.EMPLOYEE, ROLES.CLIENT), getProjectStructure);
+projectRouter.get('/:projectId/structure', requireRole(ROLES.MASTER_ADMIN, ROLES.ADMIN, ROLES.EMPLOYEE, ROLES.CLIENT, ROLES.MOBILE_USER), getProjectStructure);
 
 module.exports = { projectRouter };
